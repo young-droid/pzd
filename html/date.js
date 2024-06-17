e45234a020db4da9fdd56605d787b676700fc0d9
young-droid <young.yj.cho@gmail.com>
2024-06-18 07:31:40 +0900

    create html folder

---
+++ html/date.js
@@ -0,0 +1,9 @@
+var today = new Date();
+
+var year = today.getFullYear();
+var month = ('0' + (today.getMonth() + 1)).slice(-2);
+var day = ('0' + today.getDate()).slice(-2);
+
+var dateString = year + '-' + month + '-' + day;
+
+console.log(dateString);
