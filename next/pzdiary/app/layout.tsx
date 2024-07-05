c739cc98a212b3c5b906499e9d3e35fd9c6ddcaf
young-droid <young.yj.cho@gmail.com>
2024-07-06 05:39:44 +0900

    login from pzdiary repo

--- next/pzdiary/app/layout.tsx
+++ next/pzdiary/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import './globals.css';
+import Provider from "./provider";
+
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +21,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <><Provider>
+          {children}
+        </Provider>
+        </>
     </html>
   );
 }
